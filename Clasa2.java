b3eec8ea2fe1980c232652ff9171a891142166ad
VladislavMorozan <vlad6morozan@gmail.com>
2016-03-17 13:38:11 +0200

    Branch1 commit

--- Clasa2.java
+++ Clasa2.java
@@ -1,6 +1,6 @@
 public class Clasa2{
 	static void test()
-	{System.out.println("Eu sunt o metoda");}
+	{System.out.println("Eu sunt o metoda pentru Branch1");}
 	public static void main(String[] args){
 		test();
 	}
